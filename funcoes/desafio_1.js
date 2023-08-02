e5a22cae8f8848cd11b1955cca7d747dabf43e6c
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-08-02 06:22:34 -0300

    finalizando estudos de hoje

---
+++ funcoes/desafio_1.js
@@ -0,0 +1,32 @@
+function somar(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c
+        }
+    }
+}
+
+const somarAB = somar(3)(4)
+console.log(somarAB(13))
+console.log(somar(13)(20)(30))
+
+
+
+function calcular(x) {
+    return function(y) {
+        return function(fn) {
+            return fn(x, y)
+        }
+    }
+}
+
+function subtrair(a ,b) {
+    return a - b 
+}
+
+function multiplicar(a, b) {
+    return a * b 
+}
+
+const resultado1 = calcular(10)(5)(subtrair)
+console.log(resultado1)
