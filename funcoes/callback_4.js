8498362cdd2f040a65d74aadc0084919938c25d2
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-08-14 05:56:51 -0300

    revisando map e filter

---
+++ funcoes/callback_4.js
@@ -0,0 +1,23 @@
+
+const carrinho = [
+    {nome: 'caneta',qtde: 10, preco: 7.99},
+    {nome: 'impressora', qtde:0, preco: 649.50},
+    {nome: 'caderno', qtde:4, preco: 27.10},
+    {nome: 'lapis', qtde:3, preco: 5.82},
+    {nome: 'tesoura', qtde:1, preco: 19.20},
+]
+
+//const qtdeMaiorQueZero = item => item.qtde > 0
+//const itensValido = carrinho.filter(qtdeMaiorQueZero)
+//console.log(itensValido)
+
+
+const getNome = item => item.nome
+const qtdeMaiorQueZero = item => item.qtde > 0
+// const qtdeMaiorIgualAZero = item => item.qtde >= 0
+// const qtdeMuitoGrande = item => item.qtde >= 1000
+
+const nomeItensValido = carrinho
+   .filter(qtdeMaiorQueZero)
+   .map(getNome)
+console.log(nomeItensValido)
