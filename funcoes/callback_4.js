373976a1c0d9eddbc96f82b9a37f6ede377538bb
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-08-14 06:47:10 -0300

    finalizando revisão do map e filter

--- funcoes/callback_4.js
+++ funcoes/callback_4.js
@@ -21,3 +21,20 @@ const nomeItensValido = carrinho
    .filter(qtdeMaiorQueZero)
    .map(getNome)
 console.log(nomeItensValido)
+
+Array.prototype.meuFilter = function(fn) {
+    const novoArray = []
+    for(let i = 0; i < this.length; i++) {
+        if(fn(this[i], i, this)) {
+            novoArray.push(this[i])
+        }
+    }
+
+    return novoArray
+}
+
+const nomeItensValido2 = carrinho
+   .meuFilter(qtdeMaiorQueZero)
+   .map(getNome)
+
+console.log(nomeItensValido2)
