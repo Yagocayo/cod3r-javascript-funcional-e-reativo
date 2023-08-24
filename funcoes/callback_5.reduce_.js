944273166d3b7e0765346404501fb8e11de92b21
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-08-24 07:01:29 -0300

    finalizand revisao de reduce,map e filter

--- funcoes/callback_5.reduce_.js
+++ funcoes/callback_5.reduce_.js
@@ -46,7 +46,7 @@ o map transformar um array em um outro array com a mesma quantidade e indices mo
 
 ja o filter ele filtra de um array para outro array passando apenas o que foi mandado pela função
 
-reduce pode transformar o array em qualquer outra coisa o que vai determinar a tranformação vai ser a função
+reduce pode transformar o array em qualquer outra coisa o quem vai determinar a tranformação vai ser a função
 
 */
 
@@ -58,12 +58,47 @@ const carrinho = [
     {nome: 'tesoura', qtde:1, preco: 19.20},
 ]
 
+/*const getTotal = item => item.qtde * item.preco
+const somar = (acc, el) => {
+    console.log(acc,el)
+    return acc + el
+}
+
+const totais = carrinho.map(getTotal)
+
+console.log(totais)
+const totalGeral = totais.reduce(somar,0)// podemos passar o valor inicial ou nao passar ex: .reduce(somar, 0)
+
+console.log(totalGeral)*/
+
+
+
 const getTotal = item => item.qtde * item.preco
 const somar = (acc, el) => acc + el
 
 
 const totalGeral = carrinho
       .map(getTotal)
-      .reduce(somar)// seguno parametro e o inicial
+      .reduce(somar)// podemos passar o valor inicial ou nao passar ex: .reduce(somar, 0)
 
 console.log(totalGeral)
+
+Array.prototype.meuReduce = function(fn, inicial) {
+    let acc = inicial
+    for(let i = 0; i < this.length; i++) {
+        if(!acc && i === 0) {
+            acc = this[i]
+        } else {
+            acc = fn(acc, this[i], i, this)
+        }
+    
+    }
+
+    return acc
+}
+
+const totalGeral2 = carrinho
+      .map(getTotal)
+      .meuReduce(somar)// podemos passar o valor inicial ou nao passar ex: .reduce(somar, 0)
+
+console.log(totalGeral2)
--- funcoes/desafio_2.js
+++ funcoes/desafio_2.js
@@ -12,18 +12,56 @@ const carrinho = [
 //2. qdte * preco -> total
 //3. media totais
 
-const media = carrinho
+/*const media = carrinho
     .filter(item => item.fragil) //vai filtrar os verdadeiros e falsos
     .map(item => item.qtde * item.preco)// vai transformar o item fazeno a mutiplicação
-    .reduce(acc, el) => {
+    .reduce((acc, el) => {
         const novaQtde = acc.qtde + 1
         const novoTotal = acc.total + el
+        console.log(acc, el)
         return {
             qtde: novaQtde ,
             total: novoTotal,
             media: novoTotal / novaQtde
         }
 
-    }{qtde: 0, total: 0, media:0}
+    }, {qtde: 0, total: 0, media:0})
+    .media
 
-console.log(media)
+console.log(media)*/
+
+Array.prototype.meuReduce = function(fn, inicial) {
+    let acc = inicial
+    for(let i = 0; i < this.length; i++) {
+        if(!acc && i === 0) {
+            acc = this[i]
+        } else {
+            acc = fn(acc, this[i], i, this)
+        }
+    
+    }
+
+    return acc
+}
+
+const fragil = item => item.fragil
+const getTotal = item => item.qtde * item.preco
+const getMedia = (acc, el) => {
+    const novaQtde = acc.qtde + 1
+    const novoTotal = acc.total + el
+    return {
+        qtde: novaQtde,
+        total: novoTotal,
+        media: novoTotal / novaQtde
+    }
+}
+
+const mediaInicial = {qtde: 0, total: 0, media: 0 }
+
+const media = carrinho
+      .filter(fragil)
+      .map(getTotal)
+      .meuReduce(getMedia, mediaInicial )
+      .media
+
+console.log(`A média é ${media}!`)
