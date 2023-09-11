834d98846755aaf9ba4960cd975d2a7d3c31acad
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-09-11 07:14:11 -0300

    revisando oo em js

--- funcoes/oo_1.js
+++ funcoes/oo_1.js
@@ -1,5 +1,19 @@
-function Produto(nome, preco) {
+//sempre que colocamos o this.algumacoisa, voce esta clocando esse atributo dentro da instancia gerada, sempre que chama o new a partir da função vai ser criada uma instancia e essa instacia vai ser devolvida como resposta da chamada do new, e para acessar os atributos usamos a palavra this.
+
+function Produto(nome, preco, desc = 0.50) {
+    this.nome = nome 
+    this.preco = preco
+    this.desc = desc
+
+    this.precoFinal = function() {
+        return this.preco * (1 - this.desc)
+    }
 
 }
 
-const
+const p1 = new Produto('caneta', 10)
+console.log(p1.nome)
+
+const p2 = new Produto('geladeira', 3000)
+console.log(p2.preco)
+console.log(p2.precoFinal())
---
+++ funcoes/oo_2.js
@@ -0,0 +1,33 @@
+class Produto {
+
+    constructor(nome, preco, desc = 0.5) {
+        this.nome = nome
+        this.preco = preco
+        this.desc = desc
+    }
+
+    get nome() {
+        return `Produto:${this._nome}`
+    }
+
+    set nome(novoNome) {
+        this._nome = novoNome.toUpperCase()
+    }
+    //  precoFinal()
+    get precoFinal() { // usando get
+        return this.preco * (1 - this.desc)
+    }
+}
+
+const p1 = new Produto('caneta', 10)
+//p1.nome = 'caneta'
+p1.preco = - 20
+console.log(p1.nome)
+console.log(p1.preco)
+
+const p2 = new Produto('geladeira', 10000, 0.8)
+console.log(p2.preco)
+//console.log(p2.precoFinal())
+console.log(p2.precoFinal) // usando get
+
+console.log(typeof Produto)
