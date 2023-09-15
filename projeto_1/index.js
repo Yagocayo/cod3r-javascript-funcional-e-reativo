2d664b89552bb330ab8179b3b0f08b8f97ff7b0d
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-09-15 05:04:23 -0300

    fazeno leitura de arquivos com js

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -1,3 +1,7 @@
+const path = require('path')
 const fn = require('./funcoes')
 
-fn.lerDiretorio()
+const caminho = path.join(__dirname,'..', 'dados', 'legendas')
+
+fn.lerDiretorio(caminho)
+.then(console.log)
