ac85a838b0c697d4fa6948012274b231d91a4ebe
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-10-16 05:58:03 -0300

    revisando aulas funçoes

--- funcoes/basico_1.js
+++ funcoes/basico_1.js
@@ -26,3 +26,33 @@ console.log(resultado)
 resultado = somar(3)
 console.log(resultado)
 
+/*revisão
+
+//function Declaration
+function bomDia() {
+    console.log('bom dia')
+}
+
+bomDia()
+
+//function expression
+const boaTarde = function () {
+    console.log('boa tarde!') 
+}
+
+boaTarde()
+
+function somar(a, b) {
+    return a + b
+}
+
+let resultado = somar(3, 4) // soma os parametros passados
+console.log(resultado)
+
+resultado = somar(3, 5, 6, 7)// javascript vai ignorar o restante dos parametros que não foram passados
+console.log(resultado)
+
+resultado = somar(3)
+console.log(resultado)
+
+*/
