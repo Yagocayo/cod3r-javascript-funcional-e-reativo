876f2fe06c4b35e025a8321423644ea971f33d8e
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-10-23 19:42:22 -0300

    revisando reduce e promise

--- funcoes/desafio_3_promise.js
+++ funcoes/desafio_3_promise.js
@@ -1,3 +1,22 @@
+/*const fs = require('fs')
+const path = require('path')
+
+function lerArquivo(caminho) {
+    return new Promise(resolve => {
+        fs.readFile(caminho, function(_, conteudo) {
+            resolve(conteudo.toString())
+        })
+    })
+}
+
+const caminho = path.join(__dirname, 'dados.txt')
+
+lerArquivo(caminho)
+    .then(conteudo => conteudo.split('\n'))
+    .then(linhas => linhas.join(','))
+    .then(conteudo => `O valor final é: ${conteudo}`)
+    .then(console.log) */
+
 const fs = require('fs')
 const path = require('path')
 
@@ -16,3 +35,4 @@ lerArquivo(caminho)
     .then(linhas => linhas.join(','))
     .then(conteudo => `O valor final é: ${conteudo}`)
     .then(console.log)
+
