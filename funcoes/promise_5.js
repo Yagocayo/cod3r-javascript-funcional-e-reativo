8d3026cc75b2b207c3c553805e195d4db6310aeb
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-11-01 07:17:36 -0300

    revisando promise e motodo .then

--- funcoes/promise_5.js
+++ funcoes/promise_5.js
@@ -1,4 +1,4 @@
-function funcionarOuNao(valor, chanceErro) {
+/*function funcionarOuNao(valor, chanceErro) {
     return new Promise((resolve, reject) => {
         if(Math.random() < chanceErro) {
             reject('Ocorreu um erro!')
@@ -9,5 +9,19 @@ function funcionarOuNao(valor, chanceErro) {
 }
 funcionarOuNao('Testando...', 0.9)
     .then(v => console.log(`Valor: ${v}`))
+    .catch(err => console.log(`Erro: ${err}`))
+    .then(() => console.log('Fim')) */
+
+function funcionarOuNao(valor, chanceErro) {
+    return new Promise((resolve, reject) => {
+        if(Math.random() < chanceErro) {
+            reject('Ocorreu um erro!')
+        } else {
+            resolve(valor)
+        }
+    })
+}
+funcionarOuNao('Testando...', 0.9)
+    .then(v => console.log(`Valor: ${v}`)) //se tratar o erro dentro de um .then ele nao vai chamar o catch 
     .catch(err => console.log(`Erro: ${err}`))
     .then(() => console.log('Fim'))
