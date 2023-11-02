cc30b40f50b3f168204756a3cfd787fbd825955a
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-11-02 06:36:53 -0300

    revisano oo em javascript e async/aweit

--- funcoes/async_await_2.js
+++ funcoes/async_await_2.js
@@ -1,3 +1,40 @@
+/*function gerarNumeros(min, max, numerosProibidos) {
+    if(min > max) [max, min] = [min, max]
+    return new Promise((resolve, reject) => {
+        const fator = max - min + 1
+        const aleatorio = parseInt(Math.random() * fator) + min
+        if(numerosProibidos.includes(aleatorio)) {
+            reject('Numero repetido!')
+        } else{
+        resolve(aleatorio)
+        }
+    })
+ } 
+
+ 
+
+ async function gerarMegaSena(qtdeNumeros, tentativas = 1){
+    try {
+        const numeros = []
+        for(let _ of Array(qtdeNumeros).fill()) {
+            numeros.push(await gerarNumeros(1, 60, numeros))
+        }
+        return numeros
+    } catch(e) {
+        if(tentativas > 10){
+            throw "Não deu certo!"
+        } else {
+            return gerarMegaSena(qtdeNumeros, tentativas + 1)
+        }
+        
+    }
+ }
+
+ gerarMegaSena(8)
+    .then(console.log)
+    .catch(console.log) */
+
+
 function gerarNumeros(min, max, numerosProibidos) {
     if(min > max) [max, min] = [min, max]
     return new Promise((resolve, reject) => {
--- funcoes/oo_1.js
+++ funcoes/oo_1.js
@@ -1,4 +1,25 @@
-//sempre que colocamos o this.algumacoisa, voce esta clocando esse atributo dentro da instancia gerada, sempre que chama o new a partir da função vai ser criada uma instancia e essa instacia vai ser devolvida como resposta da chamada do new, e para acessar os atributos usamos a palavra this.
+/*//sempre que colocamos o this.algumacoisa, voce esta colocando esse atributo dentro da instancia gerada, sempre que chama o new a partir da função vai ser criada uma instancia e essa instacia vai ser devolvida como resposta da chamada do new, e para acessar os atributos usamos a palavra this.
+
+function Produto(nome, preco, desc = 0.50) {
+    this.nome = nome 
+    this.preco = preco
+    this.desc = desc
+
+    this.precoFinal = function() {
+        return this.preco * (1 - this.desc)
+    }
+
+}
+
+const p1 = new Produto('caneta', 10)
+console.log(p1.nome)
+
+const p2 = new Produto('geladeira', 3000)
+console.log(p2.preco)
+console.log(p2.precoFinal())*/
+
+
+//sempre que colocamos o this.algumacoisa, voce esta colocando esse atributo dentro da instancia gerada, sempre que chama o new a partir da função vai ser criada uma instancia e essa instacia vai ser devolvida como resposta da chamada do new, e para acessar os atributos usamos a palavra this.
 
 function Produto(nome, preco, desc = 0.50) {
     this.nome = nome 
@@ -17,3 +38,4 @@ console.log(p1.nome)
 const p2 = new Produto('geladeira', 3000)
 console.log(p2.preco)
 console.log(p2.precoFinal())
+
