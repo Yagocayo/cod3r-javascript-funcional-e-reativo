a77580ed7e2c59e386e52e2754a109f69019432e
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-11-21 06:46:53 -0300

    revisando funcao pura e impura no js

--- funcional/funcao_pura2.js
+++ funcional/funcao_pura2.js
@@ -1,3 +1,8 @@
+/*
+  Uma função pura é uma função em que o valor
+de retorno é determinado APENAS por seua valores
+de entrada, sem efeitos colaterais observaveis
+*/
 
 function gerarNumeroAleatorio(min, max) {
     const fator = max - min + 1
